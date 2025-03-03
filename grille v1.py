3ef33234d764fb8a29512b52c73a5467ae7243c3
alicemjn <alicemejane@hotmail.com>
2025-03-03 16:47:12 +0100

    grille 4x4

---
+++ grille v1.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+HEIGHT = 500
+WIDTH = 500
+largeur_case = WIDTH // 4
+hauteur_case = HEIGHT // 4
+
+racine = tk.Tk() # Création de la fenêtre racine
+canvas = tk.Canvas(racine, bg="gray", height=HEIGHT, width=WIDTH)
+canvas.grid()
+for i in range(4):
+    for j in range(4):
+        canvas.create_rectangle((i*largeur_case, j*hauteur_case),
+                ((i+1)*largeur_case, (j+1)*hauteur_case))
+racine.mainloop() # Lancement de la boucle principale
