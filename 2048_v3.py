d9a1197c0a7d91b451b796e1a4350880a47b26eb
erwan.maugeri <pinee_sacamain07@icloud.com>
2025-03-14 18:04:08 +0100

    2048_v3

--- 2048_v3.py
+++ 2048_v3.py
@@ -6,35 +6,42 @@ import tkinter as tk
 
 racine = tk.Tk()
 
-def afficheCarre(carre):
+# environnement python
+
+def afficheCarre(carre): # afficher la matric dans le terminal
     for i in range(0, len(carre)):
         print(carre[i])
 
+# matrice
 plateau = [[0,2,0,2],
-           [0,0,0,2],
-           [0,0,0,2],
-           [2,2,2,0]]
-LENGTH = 4
+           [0,0,0,16],
+           [0,0,0,32],
+           [4,2,2,0]]
 
+LENGTH = 4 # nombres de colonnes
+
+# foctions de déplacement
 def gauche(event):
     for n in range(LENGTH):
         for i in range(LENGTH):
-            if plateau[n][i] == 0:
-                pass
-            else:
+            if plateau[n][i] != 0:
                 b = i
                 while b>0 and plateau[n][b-1] == 0:
                     plateau[n][b-1] = plateau[n][b]
                     plateau[n][b] = 0
                     b -= 1
+    # regarder si il faut combiner
+    elem_a_suppr = []
+    i = 0
+    while i < LENGTH:
+        if plateau[n][i] == plateau[n][i+1]:
+            plateau[n][i] = plateau[n][i+1]
     afficheCarre(plateau)
 
-def droite(event):
+def droite(event): 
     for n in range(LENGTH):
         for i in range(LENGTH - 1, -1, -1):
-            if plateau[n][i] == 0:
-                pass
-            else:
+            if plateau[n][i] != 0:
                 b = i
                 while b < LENGTH-1 and plateau[n][b+1] == 0:
                     plateau[n][b+1] = plateau[n][b]
@@ -45,25 +52,22 @@ def droite(event):
 def haut(event):
     for i in range(LENGTH):
         for n in range(LENGTH):
-            if plateau[n][i] == 0:
-                pass
-            else:
+            if plateau[n][i] != 0:
                 b = n
-                while b > 0 and plateau[b - 1][i] == 0:
-                    plateau[b - 1][i] = plateau[b][i]
+                while b > 0 and plateau[b-1][i] == 0:
+                    plateau[b-1][i] = plateau[b][i]
                     plateau[b][i] = 0
                     b -= 1
     afficheCarre(plateau)
 
 def bas(event):
+    # déplacer
     for i in range(LENGTH):
         for n in range(LENGTH - 1, -1, -1):
-            if plateau[n][i] == 0:
-                pass
-            else:
+            if plateau[n][i] != 0:
                 b = n
                 while b < LENGTH - 1 and plateau[b + 1][i] == 0:
-                    plateau[b + 1][i] = plateau[b][i]
+                    plateau[b+1][i] = plateau[b][i]
                     plateau[b][i] = 0
                     b += 1
     afficheCarre(plateau)
