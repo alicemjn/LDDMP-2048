7c958338dd2a0b567cf089b5d5caa53da70828da
danisha05 <danishaalpen@gmail.com>
2025-03-16 18:41:05 -0200

    transpose droit

--- transpose_matrix_DANISHA.py
+++ transpose_matrix_DANISHA.py
@@ -12,3 +12,31 @@ matrix_transpose = transpose(matrix)
 print("transpose")
 for line in matrix_transpose:
     print(line)
+
+def transpose(matrix):
+    taille = len(matrix)
+    return [[matrix[j][i] for j in range(taille)] for i in range(taille)]
+
+matrix = [[4,4,4,4],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+print("matrix")
+for line in matrix:
+    print(line)
+
+def transpose_right(matrix):
+    return[list(reversed(row)) for row in transpose(matrix)]
+
+matrix_transpose = transpose(matrix)
+print("transpose")
+for line in matrix_transpose:
+    print(line)
+
+matrix_transpose = transpose(matrix_transpose)
+print("transpose gauche:")   
+for line in matrix_transpose:
+    print(line) 
+
+matrix_transpose_right = transpose_right(matrix_transpose)
+print("transpose droite:")   
+for line in matrix_transpose_right:
+    print(line)     
