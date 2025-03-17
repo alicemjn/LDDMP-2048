a986f8b17d2dd10c41576f77115de7d64af2b9ef
erwan.maugeri <pinee_sacamain07@icloud.com>
2025-03-17 14:48:54 +0100

    hello

--- transpose_matrix_DANISHA.py
+++ transpose_matrix_DANISHA.py
@@ -8,21 +8,6 @@ print("matrix")
 for line in matrix:
     print(line)
 
-matrix_transpose = transpose(matrix)
-print("transpose")
-for line in matrix_transpose:
-    print(line)
-
-def transpose(matrix):
-    taille = len(matrix)
-    return [[matrix[j][i] for j in range(taille)] for i in range(taille)]
-
-matrix = [[4,4,4,4],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-
-print("matrix")
-for line in matrix:
-    print(line)
-
 def transpose_right(matrix):
     return[list(reversed(row)) for row in transpose(matrix)]
 
