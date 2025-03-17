bf8b81b70dad98c3e0d31970ed280fc3c92c3596
erwan.maugeri <pinee_sacamain07@icloud.com>
2025-03-17 08:18:12 +0100

    ps

--- erwan/2048_V4/2048.py
+++ erwan/2048_V4/2048.py
@@ -6,15 +6,18 @@
 import tkinter as tk
 import mouvements as mv
 import matrice as mx
-import time
 
 # affichage de la matrice
 
 def afficher(grille):
+    # afficher la matrice dans le terminal
     for line in grille:
         print(line)
-
-# directions
+    # afficher la matrice dans la fenetre tkinter
+    btn1.config(text=grille[0])
+    btn2.config(text=grille[1])
+    btn3.config(text=grille[2])
+    btn4.config(text=grille[3])
 
 def fleche(event, MAJ=None, c=0):
     assert event in ("gauche", "droite", "haut", "bas"), "La fonction ne reçoit pas cet argument"
@@ -36,8 +39,7 @@ def fleche(event, MAJ=None, c=0):
             MAJ = mx.transpose(MAJ)
             MAJ = mx.transpose(MAJ)
         afficher(MAJ)
-        time.sleep(0.1)
-        fleche(event, MAJ, c=c+1)
+        racine.after(100, lambda: fleche(event, MAJ, c=c+1))
     else:
         if MAJ == grille:
             # meme matrice au début et à la fin
@@ -54,16 +56,34 @@ def fleche(event, MAJ=None, c=0):
 grille = [[0,0,0,0],
           [0,0,0,0],
           [0,0,0,0],
-          [0,0,0,1]]
+          [0,0,0,0]]
 
 # éléments visuels (tkinter)
 
 racine = tk.Tk()
 racine.title('2048')
+racine.config(padx="50px", pady="20px")
+
+btn1 = tk.Label(racine, text="", font=("system", 48))
+btn2 = tk.Label(racine, text="", font=("system", 48))
+btn3 = tk.Label(racine, text="", font=("system", 48))
+btn4 = tk.Label(racine, text="", font=("system", 48))
+
+btn1.grid(row=0, column=0)
+btn2.grid(row=1, column=0)
+btn3.grid(row=2, column=0)
+btn4.grid(row=3, column=0)
 
 racine.bind('<Up>', lambda event: fleche('haut'))
 racine.bind('<Down>', lambda event: fleche('bas'))
 racine.bind('<Left>', lambda event: fleche('gauche'))
 racine.bind('<Right>', lambda event: fleche('droite'))
+
+# initialisation
+
+mx.cube(grille)
+afficher(grille)
+
+# boucle
 
 racine.mainloop()
