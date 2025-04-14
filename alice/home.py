8584d34367410b4011f0aad0efa13951fa99f79c
alicemjn <alicemejane@hotmail.com>
2025-04-14 15:29:42 +0200

    modifications

--- alice/home.py
+++ alice/home.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+def standard () :
+    
+    
+
 racine = tk.Tk()
 racine.title('variations')
 
@@ -15,7 +19,7 @@ huitxhuit.place(x=215 , y=10, width=180, height=130)
 custom = tk.Button(racine, text="CUSTOM", font=("#756e66", 20, "bold"), bg="#ebe0cb", fg="white")
 custom.place(x=215 , y=155, width=180, height=130)
 
-competitif = tk.Button(racine, text="COMPETITIF", font=("#756e66", 20, "bold"), bg="#ebe0cb", fg="white")
+competitif = tk.Button(racine, text="COMPETITIF", font=("#756e66", 18, "bold"), bg="#ebe0cb", fg="white")
 competitif.place(x=15 , y=155, width=180, height=130)
 
 racine.mainloop()
