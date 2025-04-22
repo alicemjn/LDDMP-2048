314ab8dd67997daab18999a2cdb445864c8657db
danisha05 <danishaalpen@gmail.com>
2025-04-22 11:46:36 -0100

    max value

--- Danisha/score.py
+++ Danisha/score.py
@@ -8,9 +8,14 @@ def score(grille):
     
     assert isinstance(grille,list)and all(isinstance(row,list)for row in grille), 'Le nombre n\'est pas une matrice !'
     score=0
+    max_value=0
     for row in grille:
         for value in row:
           score+=value
-    return score    
-print(score(grille))      
+          if value>max_value:
+             max_value=value
+
+    return score ,max_value   
+print(score(grille))   
+
                 
