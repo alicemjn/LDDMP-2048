0b68bcdb32d25adc2f152f5833d70def2b4f3fac
LaurenePan <laurene.pan0727@gmail.com>
2025-05-05 00:59:50 +0200

    hello

--- espaces de travail/Laurene/robot.py
+++ espaces de travail/Laurene/robot.py
@@ -1,87 +1,124 @@
 import random as Ashkan 
 
 
-M=[[0,0,0,0],
+M=[[2,0,0,25],
    [0,0,0,0],
-   [0,4,5,8],
-   [2,8,16,32]]
+   [0,0,0,0],
+   [15,0,39,0]]
 
 
 compteur_mouvement={
-    "haut":3,
-    "bas":38,
-    "gauche":4,
-    "droite":23}
+    "haut":123,
+    "bas":1,
+    "gauche":5,
+    "droite}":23}
 
 
-def compteur (mouvement):
+
+        
+def cube(M,compteur_mouvement, mouvement) :  
+#fonction compteur rajoute dans le dico le mouvement effectuer 
     if mouvement in compteur_mouvement :
         compteur_mouvement[mouvement]+=1
-
-def valeur( M , i , j ):
-     ligne=len(M)
-     if i +1 < ligne:
-          dessous=M[i+1][j]
-          if dessous ==2:
-               M[i][j]=4
-          elif dessous ==4:
-               M[i][j]=2
-          else:
-               M[i][j]=2 if Ashkan.randint(1,10)<=9 else 4
+        trie_cle=sorted(compteur_mouvement, key=lambda cle: compteur_mouvement[1])
                
 
-def cube(M,compteur_mouvement):
-   a =  max ( compteur_mouvement, key= lambda cle :compteur_mouvement [cle] )
-   if a == "bas":
+# decide de lapparition du cube si le moouvementle plus frequent est bas et si le mouvement 
+# effctuer est bas                 
+    a =   max ( compteur_mouvement, key= lambda cle :compteur_mouvement [cle] )
+    b =   min ( compteur_mouvement, key= lambda cle :compteur_mouvement [cle] )
+    c =   trie_cle[1]
+    
+
+    if a == "bas" and mouvement=="bas" :
        for i in range(len(M)-1,-1,-1):  # Parcours des lignes de bas en haut
            case_vide=[]
            ligne= M[i]
            for j in range (len(ligne)):
-                  if ligne[j]==0:
+                if ligne[j]==0:
                     case_vide.append(j)
            if case_vide:
-                    index_case=Ashkan.randint(0,len(case_vide)-1)
-                    valeur (M, i , index_case)
-                    print (M)
-                    break
+                index_case=Ashkan.randint(0,len(case_vide)-1)
+                index_case=case_vide[index_case]
+                if i < len(M) - 1:
+                    dessous=M[i+1][j]
+                    if dessous ==2:
+                        M[i][index_case]=4
+                    elif dessous ==4:
+                        M[i][index_case]=2
+                    else:
+                        M[i][index_case]=2 if Ashkan.randint(1,10)<=9 else 4
+                else:
+                    M[i][index_case] = 2 if Ashkan.randint(1, 10) <= 9 else 4
+
+                print (M)
+                break
            
-       
-    
-
-# afficher la matrice
-
-
-
-def cube_max(M):
-    ligne = len(M)
-    colone = len(M[0])
-
-    for i in range(1, ligne):  
+#decide de la position du cube si le mouvement le moins frquant est haut et le mouvement effectuer est haut           
+  
+    if mouvement =="haut" and b=="haut" :   
+      ligne = len(M)
+      colone = len(M[0])
+      for i in range(1, ligne):  
         if 0 in M[i]:  
             max_val = -1
             coord_ligne = None
             coord_colone=None
-
             for j in range(colone):
                 if M[i][j] == 0 and M[i - 1][j] > max_val:  
                     max_val = M[i - 1][j]
                     coord_ligne = i - 1 
                     coord_colone=j
-            if coord_ligne and coord_colone:
+            if coord_ligne is not None and coord_colone is not None:
                 M[coord_ligne+1][coord_colone]=2 if Ashkan.randint(1,10)<=9 else 4
                 print(M)
-                break 
-               
- 
-   
-def transpose(M):
-    taille = len(M)
-    return [[M[j][i] for j in range(taille)] for i in range(taille)]
+                break
+
+# decide de lapparition du cube si le moouvementle plus frequent est haut et si le mouvement effctuer est haut
+
+    if a == "haut" and mouvement == "haut":
+        for i in range(len(M)):  
+            case_vide = []
+            ligne = M[i]
+            for j in range(len(ligne)):
+                if ligne[j] == 0:
+                    case_vide.append(j)
+            if case_vide:
+                index_case = Ashkan.randint(0, len(case_vide) - 1)
+                index_case = case_vide[index_case] 
+                if i > 0:
+                    dessus = M[i - 1][index_case]
+                    if dessous == 2:
+                        M[i][index_case] = 4
+                    elif dessous == 4:
+                        M[i][index_case] = 2
+                    else:
+                        M[i][index_case] = 2 if Ashkan.randint(1, 10) <= 9 else 4
+                else:
+                    M[i][index_case] = 2 if Ashkan.randint(1, 10) <= 9 else 4
+
+                print(M)
+                break    
+#decide de la position du cube si le mouvement le moins frequent est bas et le mouvement effectuer est bas          
     
+    if mouvement == "bas" and b == "bas":   
+        ligne = len(M)
+        colone = len(M[0])
+        for i in range(ligne - 2, -1, -1):  # Parcours des lignes de bas en haut
+            if 0 in M[i]:  
+                max_val = -1
+                coord_ligne = None
+                coord_colone = None
+                for j in range(colone):
+                    if M[i][j] == 0 and M[i + 1][j] > max_val:  # Comparaison avec la ligne en dessous
+                        max_val = M[i + 1][j]
+                        coord_ligne = i + 1 
+                        coord_colone = j
+                if coord_ligne is not None and coord_colone is not None:
+                    M[coord_ligne - 1][coord_colone] = 2 if Ashkan.randint(1, 10) <= 9 else 4
+                    print(M)
+                    break
 
-transpose(M)
 
-print(M[0])
-print(M[1])
-print(M[2])
-print(M[3])
+cube(M,compteur_mouvement,"bas")
+
