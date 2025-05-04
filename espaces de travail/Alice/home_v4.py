f65a957d07e8d327a3f900c3f35bd71994b0cc28
erwan.maugeri <pinee_sacamain07@icloud.com>
2025-05-04 15:11:30 +0200

    home couleurs

--- espaces de travail/Alice/home_v4.py
+++ espaces de travail/Alice/home_v4.py
@@ -21,18 +21,16 @@ racine = tk.Tk()
 racine.configure(height=520, width=520)
 racine.title('home')
 
+# canvas avec titre 2048 et grille automatique
 
 overlay_canvas = tk.Canvas(racine, bg="red", highlightthickness=0)
 overlay_canvas.place(x=0, y=0, width=520, height=520)
 
+# titre 2048 dans le canvas
+
 overlay_canvas.create_text(260, 80, text="2048", font=("Helvetica, Arial, sans-serif", 80, 'bold'), fill=couleur["font_title"])
 
-# 2048 titre
-
-#title = tk.Label(racine, text="2048", font=("Helvetica, Arial, sans-serif", 80, "bold"), fg=couleur["font_title"])
-#title.place(x=0, y=0, height=150, width=520)
-
-# boutons code: container puis texte sup puis texte inf
+# boutons par dessus le canvas
 
     # classique
 frameclassique=tk.Frame(racine, bg=couleur["jaune"])
@@ -55,25 +53,17 @@ etendue2=tk.Label(frameetendue, text="8x8", font=("Helvetica, Arial, sans-serif"
 etendue2.place(x=0, y=38, width=180, height=35)
 
     #personnalisé
-frameperso=tk.Canvas(racine, bg=couleur["gris"], highlightthickness=0)
+frameperso=tk.Label(racine, bg=couleur["gris"])
 frameperso.place(x=66, y=255, width=180, height=85)
 
 perso1=tk.Label(frameperso, text="Personnalisé", font=("Helvetica, Arial, sans-serif", 20), bg=couleur["gris"], fg="white")
 perso1.place(x=5, y=2, width=180, height=40)
 
 perso_entrée = tk.Entry(frameperso, bg="white", fg="black", font=("Helvetica, Arial, sans-serif", 20), borderwidth=0, highlightthickness=0)
-perso_entrée.place(x=34, y=50, width=50, height=25)
+perso_entrée.place(x=40, y=40, width=70, height=30)
 
-# boutons Ok proportions 38x38
-perso_ok=tk.Label(frameperso, text="OK", font=("Helvetica, Arial, sans-serif", 13), bg="white")
-perso_ok.place(x=114, y=49)
-
-# ajout des contours boutons ok et entry (coins arrondis)
-frameperso.create_oval(110, 42, 148, 80, fill="white", width=0)
-        # on créer un rectangle et deux cercles pour le entry hauteur = 38
-frameperso.create_oval(18, 42, 56, 80, fill="white", width=0)
-frameperso.create_oval(60, 42, 98, 80, fill="white", width=0)
-frameperso.create_rectangle(37,42,80,80, fill="white", width=0)
+perso_ok=tk.Label(frameperso, text="Ok", font=("Helvetica, Arial, sans-serif", 13), bg="white")
+perso_ok.place(x=117, y=40, height=30, width=30)
 
     #competitif
 framecompetitif=tk.Frame(racine, bg=couleur["orange"])
