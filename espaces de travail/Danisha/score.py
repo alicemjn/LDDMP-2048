76a934c87966d24e65cdaf3cd04799a0d59a820d
danisha05 <danishaalpen@gmail.com>
2025-05-04 16:07:45 -0100

    score

--- espaces de travail/Danisha/score.py
+++ espaces de travail/Danisha/score.py
@@ -1,5 +1,8 @@
 # score de la grille
 
+grille=[[1,0,0,0],[2,0,0,0],[8,0,0,0],[1028,0,0,0]]
+                                       
+                                       
 def score(grille):
     assert isinstance(grille,list) and all(isinstance(row,list)for row in grille), 'Le nombre n\'est pas une matrice !'
     score = 0
