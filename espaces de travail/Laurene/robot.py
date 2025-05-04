b4003a8584825212735b659845cdb886db6b7ec6
LaurenePan <laurene.pan0727@gmail.com>
2025-05-04 16:43:48 +0200

    cacca

--- espaces de travail/Laurene/robot.py
+++ espaces de travail/Laurene/robot.py
@@ -55,3 +55,35 @@ print (M[2])
 print (M[3])
 
 
+import random as Afshkan
+
+M=[[1,7,2,2],
+   [6,9,9,20],
+   [0,0,0,2],
+   [2,8,16,32]]
+
+def cube_max(M):
+    ligne = len(M)
+    colone = len(M[0])
+
+    for i in range(1, ligne):  
+        if 0 in M[i]:  
+            max_val = -1
+            coord_ligne = None
+            coord_colone=None
+
+            for j in range(colone):
+                if M[i][j] == 0 and M[i - 1][j] > max_val:  
+                    max_val = M[i - 1][j]
+                    coord_ligne = i - 1 
+                    coord_colone=j
+            if coord_ligne and coord_colone:
+                M[coord_ligne+1][coord_colone]=2 if Afshkan.randint(1,10)<=9 else 4
+                print(M)
+                break 
+               
+ 
+   
+
+cube_max(M)
+
