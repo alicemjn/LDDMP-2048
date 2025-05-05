af48e1d6b3b8bd746eee61adf28fcc07cbaf66ec
danisha05 <danishaalpen@gmail.com>
2025-05-05 10:13:15 -0100

    jh

--- espaces de travail/Danisha/transpose_matrix_DANISHA.py
+++ espaces de travail/Danisha/transpose_matrix_DANISHA.py
@@ -6,8 +6,8 @@ matrix = [[4,4,4,4],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
 def transpose_right(matrix):
     return[list(reversed(row)) for row in transpose(matrix)]
-
-
+           
+           
 matrix_transpose = transpose(matrix)
 '''cette fonction permet de transposer la matrice a gauche'''
 print("transpose gauche:")   
